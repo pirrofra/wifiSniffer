2b11233bd3112e0df50ef0a0dbff8fc0e4d9adc2
pirrofra <pirro.francesco@gmail.com>
2020-03-20 05:16:46 +0100

    small optimization in datacleaning

--- src/dataCleaner/dataCleaner.py
+++ src/dataCleaner/dataCleaner.py
@@ -5,16 +5,18 @@ from time import sleep
 from math import sqrt
 import requests
 
+
 #1545154 unique records
 #906752 after delete Redundancies
 #860033 after outliner remove (300s)
 #862597 after outliner remove(1h)
 
-deviceManager="http://dataManager:1000/cleanData"
+deviceManager="http://data_manager:1000/cleanData"
 #deviceManager="http://127.0.0.1:5000/cleanData"
 
-#dbService=pymongo.MongoClient("tmpDatabase",27017)
+#dbService=pymongo.MongoClient("tmp_database",27017)
 dbService=pymongo.MongoClient("localhost",27017)
+
 db=dbService["wifiSniffer"]
 
 def cleaning():
@@ -22,10 +24,12 @@ def cleaning():
     deleteRedundacies()
     db.cleaned.rename("cleaning")
     removeOutliner()
-    data=db.cleaned.find({},{"_id":0})
-    data=list(data)
-    send(data)
-    db.cleaned.drop()
+    db.cleaned.rename("cleaning")
+    highestRSSI()
+    #data=db.cleaned.find({},{"_id":0})
+    #data=list(data)
+    #send(data)
+    #db.cleaned.drop()
 
 def deleteRedundacies():
     pipeline=[
@@ -35,13 +39,12 @@ def deleteRedundacies():
                 "scanner_id":"$scanner_id",
                 "timestamp":"$timestamp"
                 },
-            "rssi":{"$push":"$rssi"}}
+            "rssi":{"$max":"$rssi"}}#proviamo max al posto di push
         },
         {"$project":{
             "_id":0,
             "mac":"$_id.mac2",
             "scanner_id":"$_id.scanner_id",
-            "room":"$_id.scanner_id",
             "timestamp":"$_id.timestamp",
             "rssi":"$rssi"            
         }},
@@ -66,13 +69,15 @@ def getMinMax():
 
 
 def removeOutliner():
+    removeLowRSSI(-90)
     min,max=getMinMax()
     time=min
     while(time<=max):
-        removeOutlinerInterval(time,time+120)
+        removeOutlinerInterval(time,time+300)
         perc= ((time-min)*100)/(max-min)
         print("OUTLINER: "+str(perc)+"%...")
-        time+=120
+        time+=300
+    db.cleaning.drop()
 
 def removeOutlinerInterval(start,end):
     dic=getAvgStdDev(start,end)
@@ -81,22 +86,17 @@ def removeOutlinerInterval(start,end):
     cleaned=[]
     for element in elements:
         (avg,stddev)=dic[element["scanner_id"]]
-        newRSSI=[]
-        for rssi in element["rssi"]:
-            if(stddev!=0):
-                d=(rssi-avg)/stddev
-                if(d<=2 and d>=-2 and rssi>=-90):
-                    newRSSI.append(rssi)
-        if(len(newRSSI)!=0):
-            element["rssi"]=newRSSI
-            cleaned.append(element)
+        if(stddev!=0):
+            d=(element["rssi"]-avg)/stddev
+            if(d<=2 and d>=-2):
+                cleaned.append(element)
+
     if(len(cleaned)!=0):
         db.cleaned.insert_many(cleaned)
 
 def getAvgStdDev(start,end):
     pipeline=[
         {"$match":{"timestamp":{"$lt":end,"$gte":start}}},
-        {"$unwind":"$rssi"},
         {"$sort":SON([("scanner_id",1),("rssi",-1)])},
         {"$group":
             {"_id":"$scanner_id",
@@ -120,6 +120,57 @@ def getAvgStdDev(start,end):
         dic[el["scanner_id"]]=(m,el["stddev"])
     return dic
 
+def removeLowRSSI(low):
+    query={"rssi":{"$lt":low}}
+    db.cleaning.delete_many(query)
+
+
+def highestRSSI():
+    min,max=getMinMax()
+    time=min
+    while(time<=max):
+        highestRSSIInterval(time,time+60)
+        perc= ((time-min)*100)/(max-min)
+        print("HIGHEST RSSI: "+str(perc)+"%...")
+        time+=60
+
+def highestRSSIInterval(start,end):
+    pipeline=[
+        {"$match":{"timestamp":{"$gte":start,"$lt":end}}},
+        {"$sort":SON([("mac",1),("rssi",-1)])},
+        {"$group":{
+            "_id":"$mac",
+            "rssi":{"$max":"$rssi"},
+            "room":{"$first":"$scanner_id"}}},
+        {"$project":{
+            "mac":"$_id",
+            "rssi":"$rssi",
+            "room":"$room"}},
+        {"$out":"tmp"}]
+    db.cleaning.aggregate(pipeline,allowDiskUse=True)
+
+    join=[
+        {"$match":{"timestamp":{"$gte":start,"$lt":end}}},
+        {"$lookup":{
+            "from":"tmp",
+            "localField":"mac",
+            "foreignField":"mac",
+            "as": "room"}},
+        {"$project":{
+            "mac":"$mac",
+            "scanner_id":"$scanner_id",
+            "room":{"$arrayElemAt":["$room",0]},
+            "timestamp":"$timestamp",
+            "rssi":"$rssi"}},
+        {"$project":{
+            "mac":"$mac",
+            "scanner_id":"$scanner_id",
+            "room":"$room.room",
+            "timestamp":"$timestamp",
+            "rssi":"$rssi"}},
+        {"$merge":"cleaned"}]
+    db.cleaning.aggregate(join,allowDiskUse=True)
+
 
 def send(data):
     x = requests.post(deviceManager,json=data)
@@ -132,10 +183,8 @@ def send(data):
 def main():
     while(True):
         while(db.rawData.count()==0):
-            sleep(120)
+            sleep(300)
         cleaning()
-        sleep(120)
+        sleep(300)
 
 main()
-
-    
